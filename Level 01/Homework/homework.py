b5da390485f2074770d9c43b2431a1e483fa4393
Gabo353 <dumbadzemambo@gmail.com>
2024-09-08 15:52:08 +0400

    GOA

--- Level 01/Homework/homework.py
+++ Level 01/Homework/homework.py
@@ -1,1 +1,17 @@
-name = 'gabriel'
+#ვარიანტი 1
+name = 'Gabriel'
+surname = 'Dumbadze'
+height = '1.65'
+age = '13'
+story = 'i stepped in GOA to chase my dreams.My dream is to become successful developer.'
+print(name,surname,age,height,story)
+      
+#ვარიანტი 2
+name = "Gabriel"
+surname = "Dumbadze"
+age = 13
+height = 1.65
+story = 'i stepped in GOA to chase my dreams.My dream is to become successful developer.'
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + story)
+
+
