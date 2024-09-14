36e88ba5c13b01bab3fd918882c153c2026bb1b8
Gabo353 <dumbadzemambo@gmail.com>
2024-09-14 13:41:37 +0400

    GOA

---
+++ Level 03/Classwork/Classwork02.py
@@ -0,0 +1,8 @@
+name = "Gabriel"
+player_speed = 15
+print(name)
+print(player_speed)
+surname = "Dumbadze"
+print(name + ' ' + surname)
+
+
---
+++ Level 03/homework02/Homerwork02.py
@@ -0,0 +1,25 @@
+score = 10
+print(score)
+print('---------------------------')
+
+health = 5
+print(health)
+print('---------------------------')
+
+attacking_points = 3.5
+defence_points = 4
+print(attacking_points,defence_points)
+print('---------------------------')
+
+math_score = 10
+english_score = 10
+science_score = 8.5
+chemistry_score = 9
+history_score = 9.5
+print(math_score,english_score,science_score,chemistry_score,history_score)
+print('----------------------------')
+
+name = 'Hi! my name is gabrieli'
+surname = 'and my surname is Dumbadze.'
+age = 'im 13 years old'
+print(name,surname,age)
