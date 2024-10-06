4a1df9c08b5a9daef2042c25ce0bee0f0b027430
Gabo353 <dumbadzemambo@gmail.com>
2024-10-06 12:35:25 +0400

    GOA

---
+++ Level 08/Classwork/Classwork.py
@@ -0,0 +1,44 @@
+#!= არ უდრის true=1 false=0
+print(5 == 10)
+print(10 == 5)
+print(10 <= 5)
+print(5 <= 10)
+print(10 <= 5)
+print(10 >= 5)
+print(5 >= 10)
+print(5 != 10)
+print(10 != 5)
+
+print('---------------------------')
+#1
+misha = 20
+grisha = 10
+
+if grisha >= misha:
+   print('grisha correct')
+
+if misha >= grisha:
+   print('misha correct')
+
+print('----------------------')
+#2
+misha = 20
+grisha = 10
+print(misha >= grisha)
+
+print('-----------------------')
+
+
+#tu sadme aris false da-shi mashin aucileblad iqneba false
+#-----------------------------------
+# and aris aucilebeli piroba
+#print(false and 'hello') hello
+print(False and 'hello')
+print(True and False)# false
+print(False and True)# false
+
+#or ar aris aucilebeli piroba
+
+print(True or False) #true
+print(False or False) #false
+
