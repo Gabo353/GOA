d7730cb5e115796fd2cb773a426552f3cd0b1b60
Gabo353 <dumbadzemambo@gmail.com>
2024-10-24 18:45:38 +0400

    GOA

---
+++ On Free Leson/Classwork/Class.py
@@ -0,0 +1,26 @@
+name = 'Two tower'
+result = ''
+result += name
+
+#for i in name:
+#    result += i
+#print(result)
+
+
+# != ar udris
+ 
+#if 4<1:
+ # print('free N-pass')
+
+#else :
+#   print('4 მეტია ერთზე ტო?')
+
+
+
+num = int(input('Your number: '))
+
+if num % 2 ==0:
+  print('Your number is even')
+
+else:
+  print('Your number is not even')
