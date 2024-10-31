f0166642b9a92d25fab7a45ac7e4b14f9347291a
Gabo353 <dumbadzemambo@gmail.com>
2024-10-31 17:17:02 +0400

    GOA

---
+++ Level 15/Classwork/Classowork.py
@@ -0,0 +1,33 @@
+#user_passowrd = input('enter password: ')
+#password = '123'
+
+#while user_passowrd != password:
+ #  user_passowrd = input('try again: ')
+#print('log in succesfully ')
+
+#age = int(input('Your age: '))
+
+#if age < 18:
+ # print('lil kids discount')
+ 
+#elif age > 75:
+ # print('Damn grany discount')
+  
+
+#guess_num = int(input('Guess number: '))
+#num = 23
+
+#while guess_num != num:
+ #  guess_num = input('Try again: ')
+
+#print('You got it baby23')
+
+
+#სია-array გამოიყოფა მძიმეებით----> ,
+#              0         1       2     3   4
+#               -5       -4      -3     -2  -1 ბოლო სულ -1 ია
+#gabo_list = ['vaaj', 'davit', 'gabo',True,12]
+#print(gabo_list)
+#print(gabo_list[4])
+name = 'group 47'
+print(name[3])
