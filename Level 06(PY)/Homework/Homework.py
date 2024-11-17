2d73f48b2b92fdf725db3ffae8f089c895425db9
Gabo353 <dumbadzemambo@gmail.com>
2024-11-17 13:55:06 +0400

    GOA

---
+++ Level 06(PY)/Homework/Homework.py
@@ -0,0 +1,38 @@
+#1
+age = int(input('Your born year: '))
+sum = 2024 - age
+print(sum)
+
+print('------------------')
+#2
+width = int(input('Widht: '))
+length = int(input('length: '))
+P = width*length
+S = 2*(width+length)
+print(P)
+print(S)
+
+print('---------------------')
+#3
+kilo = int(input('Kilometers: '))
+metre = kilo*1000
+cenmet = kilo*100000
+milimetre = kilo*1000000
+print(metre)
+print(cenmet)
+print(milimetre)
+
+print('-----------------------')
+#4
+name = input('Name: ')
+surname = input('Surname: ')
+parents = input('Parents name: ')
+color = input('Fav color: ')
+car = input('Fav car: ')
+hobbi = input('Your top 3 hobbies: ')
+print(name,surname,parents,color,car,hobbi)
+
+print('------------------------')
+#5
+num = int(input('Your two symbol num: '))
+print('???')
