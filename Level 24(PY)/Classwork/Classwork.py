7ec17d2e983697990584d966d5887e9acb785723
Gabo353 <dumbadzemambo@gmail.com>
2024-11-26 18:22:53 +0400

    GOA

---
+++ Level 24(PY)/Classwork/Classwork.py
@@ -0,0 +1,18 @@
+#num = 'Me gabo var' 
+#message = "Gamarjoba,Rogor xar? {}".format(num = ''mgeli)
+#print(message)
+
+#print('-----------split---------------')
+#_string = 'shaurma ar aris xinkalze uketesi'
+#_split_string = _string.split(' ')
+#print(_split_string)
+
+
+#_string = ['jobnis' , 'jobni' , 'ar' , 'dailevao']
+#joined_string = " ! ".join(_string)
+#print(joined_string)
+
+
+_str_of_vowel = "a u d e"
+_updated_str_of_vowel = _str_of_vowel.replace(' ','-')
+_updated_str_of_vowel = _str_of_vowel.split()
