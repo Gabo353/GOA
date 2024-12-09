7ed06718ee4b8108a32f8290ed72341b1c17b5d4
Gabo353 <dumbadzemambo@gmail.com>
2024-12-09 18:25:50 +0400

    GOA

---
+++ Level 06(PY)/Classwork/Classwork.py
@@ -0,0 +1,10 @@
+#1
+name = input('Your name: ')
+surname = input('Your Surname: ')
+print(name,surname)
+
+#2
+num1 = int(input('First number: '))
+symbol = input('Any symbol: ')
+num2 = int(input('Second number: '))
+print(num1,symbol,num2)
