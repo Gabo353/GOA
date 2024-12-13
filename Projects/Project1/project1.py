d770276718c0fc27844ea35faa62b6c4cbe08fc8
Gabo353 <dumbadzemambo@gmail.com>
2024-12-13 22:35:20 +0400

    GOA

--- Projects/Project1/project1.py
+++ Projects/Project1/project1.py
@@ -1,24 +1,29 @@
-print('გამრავლების შემოკლებული ფორმულები')
-apps = str(input('Any app: '))
-first_num = int(input('First num: '))
-second_num = int(input('Second num: '))
-options = input('Any option: ')
+print('Mini phone inter face')
+print('calc = calculator')
+print('Tran=Transletor')
+apps = input('Any app: ')
+
+isrunning = True
+
+if apps == 'clac':
+  def calculator():
+        first_num = int(input('First num: '))
+        second_num = int(input('Second num: '))
+        options = input('Any math symbol: ')
+
+        if options == '/':
+          print(first_num/second_num)
+
+        elif options == '*':
+          print(first_num*second_num)    
+
+        elif options == '+':
+          print(first_num+second_num)     
+
+        elif options == '-':
+          print(first_num-second_num)  
+          
+  calculator()
 
 
-
-
-def calculator():
-  if options == '/':
-    print(first_num/second_num)
-
-  elif options == '*':
-    print(first_num*second_num)    
-
-  elif options == '+':
-    print(first_num+second_num)     
-
-  elif options == '-':
-    print(first_num-second_num)  
-
-if apps == 'calc':
-   print(calculator())
+  
