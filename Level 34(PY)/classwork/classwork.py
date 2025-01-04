1a6373546c06e4241ebb4973d3f3f40cb7ba2aad
Gabo353 <dumbadzemambo@gmail.com>
2025-01-04 15:15:11 +0400

    GOA

---
+++ Level 34(PY)/classwork/classwork.py
@@ -0,0 +1,13 @@
+#ar aris saklaso
+#points = (12,13,13,13,16,7,8)
+#print(points.count(13))
+#first,*rest = points
+#print(first) 
+#print(rest)   
+
+movies = ('Home alone','Grinch','Truman','Green book','Shrek','Cat in boots')
+Home,*rest = movies
+print(Home)
+print(rest)
+print(movies[0])
+print(movies[-1])
