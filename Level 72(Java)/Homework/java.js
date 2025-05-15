8e4ad39f8054ce8782f4207bd918de05c142bb65
Gabo353 <dumbadzemambo@gmail.com>
2025-05-15 19:05:15 +0400

    GOA

---
+++ Level 72(Java)/Homework/java.js
@@ -0,0 +1,36 @@
+//#1
+let age = 14
+console.log(age)
+
+//#2
+const name = 'Gabo'
+console.log(name)
+
+//#3
+const bb = document.getElementById('bb')
+
+function bgclick() {
+
+    bb.style.background = 'red'
+
+}
+
+//#4
+const inp = document.getElementById('inpute')
+
+function info(){
+
+    console.log(inpute.value)
+
+}
+
+//#5
+const div = document.getElementById('fuun')
+
+function change(){
+
+    fuun.style.background = 'blue'
+    fuun.style.height = '200px'
+    fuun.style.width = '200px'
+
+}
---
+++ Level 72(Java)/classwork/scrip.js
@@ -0,0 +1,24 @@
+console.log('hi')
+
+// 3 way to creat a 'Variable'
+
+//let
+//let gela = 2
+//console.log(gela)
+
+
+//const
+//const array = [1,2,3,4]
+//console.log(array)
+
+//var
+//var age = 21
+
+const body = document.getElementById('body')
+console.log(body)
+
+function bgChange(){
+
+    body.style.background = 'blue'
+
+}
