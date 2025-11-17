41391b40b69861f67022c562cfbf97107077fdac
Gabo353 <dumbadzemambo@gmail.com>
2025-11-17 20:56:09 +0400

    GOA

--- Level 116/Home/hw8.js
+++ Level 116/Home/hw8.js
@@ -7,3 +7,11 @@ for (let b of ies) {
     b.textContent = 'virir'
     
 }
+
+let count = 0
+while (count < ies.length) {
+
+    ies[count].textContent = 'virir'
+    count += 1
+
+}
---
+++ Level 117/Class/index.js
@@ -0,0 +1,8 @@
+let ies = document.getElementsByTagName("i")
+let count = 0
+while (count < ies.length) {
+
+    ies[count].textContent = 'virir'
+    count += 1
+
+}
