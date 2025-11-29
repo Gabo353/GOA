d0b67e2d6b863a42365a45f72b422b72aa1c6a10
Gabo353 <dumbadzemambo@gmail.com>
2025-11-29 22:05:09 +0400

    GOA

---
+++ Level 120/Class/index.js
@@ -0,0 +1,8 @@
+let btn_add = document.getElementById('btn_add')
+
+btn_add.addEventListener("click", function(event) {
+  
+    let element = document.createElement(event.target)
+    console.log(element)
+
+});
---
+++ Level 120/Home/index.js
@@ -0,0 +1,23 @@
+//1)გააკეტეთ ესეთი პროექტი:ტექსტის ფერის არჩევა, იყოს ფერების ჩამონათვალი დივების სახით, და ერთი ინფუთი, შემდეგ ამ ფერების დაჭერაზე ინფუთის წერის დროს მაგ  ფერის ტექსტი დაიწეროს, გამოიყენეთ event ობიუქტი
+
+let divs = document.getElementsByTagName('div')
+let input = document.getElementsByTagName('input')
+
+function blue() {
+
+    input[0].style.color = 'blue'
+
+}
+function red() {
+
+    input[0].style.color = 'red'
+
+}function yellow() {
+
+    input[0].style.color = 'yellow'
+
+}
+
+divs[1].addEventListener('click', blue)
+divs[2].addEventListener('click', red)
+divs[3].addEventListener('click', yellow)
