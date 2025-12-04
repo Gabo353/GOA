c1b02356ab6ca2a225e7dc8bab9f9c121348176f
Gabo353 <dumbadzemambo@gmail.com>
2025-12-04 21:49:41 +0400

    GOA

--- Level 121/Home/index.js
+++ Level 121/Home/index.js
@@ -1,15 +1,34 @@
 // HTML-ში შექმენით ფორმა სადაც გექნებათ სამი input ველი, სახელისთვის, გვარისთვის და ასაკისთვის. submit-ღილაკის გამოყენებით გააგზავნეთ შეყვანილი ინფორმაცია რომელიც გამოჩნდება ლისტში, თითოეული გაგზავნის შემდეგ უნდა დაემატოს ახალი ლისტი სადაც გამოიტანთ იმ ინფორმაციას რაც input-ში შეიყვანეთ. ყოველი submit ღილაკზე დაჭერის შემდეგ უნდა იქმნებოდეს ახალი ლისტი. ეს ყველაფერი გაალამაზეთ CSS-ის გამოყენებით
 
 let form = document.getElementById('form')
-document.addEventListener('submit', function(e) {
+let count = 0
+document.addEventListener('submit', function (e) {
     e.preventDefault()
     let list_name = ['Name: ', 'Surname: ', 'Age: ']
-    let arr = [e.target.name.value,e.target.surname.value,e.target.age.value]
+    let arr = [e.target.name.value, e.target.surname.value, e.target.age.value]
     let ul = document.createElement('ul')
+    
+    count++
+    for (let i = 0; i < 1; i++) {
+
+        let acc = document.createElement('span')
+        acc.textContent = `account number: ${count}`
+        
+
+        ul.appendChild(acc)
+
+    }
+
     for (let i = 0; i < arr.length; i++) {
         let li = document.createElement('li')
         li.textContent = `${list_name[i]}${arr[i]}`
+        
         ul.appendChild(li)
+        
     }
+    
+
     document.body.appendChild(ul)
 })
+
+
