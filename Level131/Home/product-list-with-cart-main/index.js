7634840d243638df402f9dd7083fb221317c6f48
Gabo353 <dumbadzemambo@gmail.com>
2026-01-25 21:26:21 +0400

    GOA

--- Level131/Home/product-list-with-cart-main/index.js
+++ Level131/Home/product-list-with-cart-main/index.js
@@ -1,99 +1,68 @@
 let btn = document.getElementsByClassName('button')
-let changed_h1 = document.getElementById('h1_2')
-
-let namme = document.getElementsByClassName('text_info_2')
-let price = document.getElementsByClassName('price')
-
-let remove_img = document.getElementById('img_special')
-let order_main_div = document.getElementById('second_main')
-let remove_text = document.getElementsByClassName('notification')[0]
-
-let count = 0
-let count_items = 0
-let order_confirm_btn = document.createElement('button')
-
-order_confirm_btn.style.width = '90%'
-order_confirm_btn.style.height = '60px'
-order_confirm_btn.style.backgroundColor = 'rgb(187, 52, 3)'
-order_confirm_btn.innerHTML = 'Confirm Order'
-order_confirm_btn.style.color = 'white'
-order_confirm_btn.style.border = 'none'
-order_confirm_btn.style.borderRadius = '50px'
-order_confirm_btn.style.fontWeight = '500'
-order_confirm_btn.style.fontSize = '16px'
-order_confirm_btn.style.position = 'absolute'
-order_confirm_btn.style.bottom = '-10%'
-order_confirm_btn.style.left = '5%'
+let remove_img = document.getElementsByClassName('added_icon')
 
 
-for (let i = 0; i < 10; i++) {
-    btn[i].addEventListener('click', function () {
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', function () {
+    let count = 1
+    this.style.backgroundColor = 'rgb(206, 59, 5)'
+    this.style.color = 'white'
+    this.textContent = ''
+    this.style.display = 'flex'
+    
+    
 
-        count += 1
-        changed_h1.innerText = `Your Cart (${count})`
+    let icon = this.getElementsByClassName('added_icon')[0]
+    if (icon) {
+      icon.remove()
+    }
 
+    let count_span = document.createElement('span')
+    count_span.textContent = ''
+    this.appendChild(count_span)
 
-        let order_div = document.createElement('div')
-        let order_info_text_div = document.createElement('div')
-        let order_span = document.createElement('span')
-        let order_amount = document.createElement('span')
-        let order_price = document.createElement('span')
-        let order_price_times_amout = document.createElement('span')
+    let plus = document.createElement('div')
+    plus.textContent = `+`
+    plus.id = 'plus_btn'
+    plus.style.border = '1px solid white'
+    plus.style.borderRadius = '50px'
+    plus.style.width = '20px'
+    plus.style.height = '20px'
+    plus.style.display = 'flex'
+    plus.style.alignContent = 'center'
+    plus.style.justifyContent = 'center'
+    plus.style.flexFlow = 'center'
+    plus.style.position = 'absolute'
+    plus.style.right = '20%'
+    
+    let minus = document.createElement('div')
+    minus.textContent = `-`
+    minus.id = 'minus_btn'
+    minus.style.border = '1px solid white'
+    minus.style.borderRadius = '50px'
+    minus.style.width = '20px'
+    minus.style.height = '20px'
+    minus.style.display = 'flex'
+    minus.style.alignContent = 'center'
+    minus.style.justifyContent = 'center'
+    minus.style.position = 'absolute'
+    minus.style.left = '20%'
 
+    btn[i].appendChild(plus)
+    btn[i].appendChild(minus)
 
-
-        order_div.style.height = '70px'
-        order_div.style.width = '90%'
-        order_div.style.backgroundColor = 'white'
-        order_div.style.borderBottom = '1px solid rgba(128, 128, 128, 0.515)'
-        order_div.style.marginLeft = '30px'
-
-        order_span.innerText = namme[i].innerText
-        order_span.style.fontSize = '18px'
-        order_span.style.fontWeight = '500'
-
-        order_amount.innerText = '1x'
-        order_amount.style.fontSize = '18px'
-        order_amount.style.color = 'rgb(206, 59, 5)'
-        order_amount.style.fontWeight = '500'
-
-        order_price.innerHTML = `@${price[i].innerText}`
-        order_price.style.color = 'rgba(128, 128, 128, 0.515)'
-        order_price.style.fontWeight = '500'
-
-        order_price_times_amout.innerHTML = `${price[i].innerText}`
-        order_price_times_amout.style.color = 'grey'
-        order_price_times_amout.style.fontWeight = '500'
-
-        order_div.style.display = 'flex'
-        order_div.style.flexDirection = 'column'
-        order_div.style.gap = '8px'
-        order_div.style.paddingTop = '25px'
-
-        order_info_text_div.style.display = 'flex'
-        order_info_text_div.style.gap = '13px'
-        order_info_text_div.style.alignItems = 'center'
-
-
-        order_div.appendChild(order_span)
-        order_info_text_div.appendChild(order_amount)
-        order_info_text_div.appendChild(order_price)
-        order_info_text_div.appendChild(order_price_times_amout)
-
-        order_div.appendChild(order_info_text_div)
-
-
-
-        remove_img.remove()
-        remove_text.remove()
-
-
-        order_main_div.appendChild(order_div)
-        if (!order_confirm_btn.isConnected) {
-            order_main_div.appendChild(order_confirm_btn)
-        }
-        
-
+    plus.addEventListener('click', function (e) {
+      e.stopPropagation();
+      count++;
+      count_span.textContent = count;
     })
 
+    minus.addEventListener('click', function (e) {
+      e.stopPropagation();
+      if (count > 1) {
+        count--;
+        count_span.textContent = count;
+      }
+    })
+  })
 }
