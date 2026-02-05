667109ec93fa80108d0846f896af489280b3fcd1
Gabo353 <dumbadzemambo@gmail.com>
2026-02-05 21:22:32 +0400

    GOA

--- Level 135/Home/rock-paper-scissors-master/index.js
+++ Level 135/Home/rock-paper-scissors-master/index.js
@@ -8,7 +8,7 @@ rules_btn.addEventListener('click', function () {
     let button = document.createElement('div')
     let span = document.createElement('span')
     let img = document.createElement('img')
-    
+
     div.style.height = '450px'
     div.style.width = '450px'
     div.style.backgroundColor = 'white'
@@ -48,14 +48,236 @@ rules_btn.addEventListener('click', function () {
         div.remove()
 
     })
+    console.log(img)
 })
 
 //------------------------------Game-------------------------------------
+
+
 
 let computer_move = Math.floor(Math.random() * 3)
 let paper = document.getElementById('choose_paper')
 let rock = document.getElementById('choose_rock')
 let scisors = document.getElementById('choose_sci')
+let stick_1 = document.getElementById('stick_1')
+let stick_2 = document.getElementById('stick_2')
+let stick_3 = document.getElementById('stick_3')
 
-let computer_choice = [rock,paper,scisors]
+let rock_src = rock.src
+let paper_src = paper.src
+let scisors_src = scisors.src
+
+
+let notification = document.getElementById('noty')
+let score = document.getElementById('score_num')
+let score_num = 0
+let me = document.getElementById('me')
+let comp = document.getElementById('comp')
+
+let computer_choice = [rock, paper, scisors]
+let computer_choice_str = ["rock", "paper", "scisors"]
+
+let my_bid = ''
+let computer_bid = computer_choice_str[computer_move]
+
+function reset_game_ui() {
+    
+    body.appendChild(rock)
+    body.appendChild(paper)
+    body.appendChild(scisors)
+    body.appendChild(stick_1)
+    body.appendChild(stick_2)
+    body.appendChild(stick_3)
+
+    
+    me.innerText = ''
+    comp.innerText = ''
+    notification.innerText = ''
+
+    
+    let old_btn = document.getElementById('again')
+    if (old_btn) old_btn.remove()
+
+    
+    rock.src = rock_src
+    paper.src = paper_src
+    scisors.src = scisors_src
+}
+
+
+function game_simulation() {
+    
+
+    if (my_bid == computer_bid) { notification.innerHTML = 'draw' }
+    else if (my_bid == 'paper' && computer_bid == 'scisors') { notification.innerText = 'You lose' }
+    else if (my_bid == 'paper' && computer_bid == 'rock') { notification.innerText = 'You win'; score_num += 1; score.innerText = score_num }
+
+    else if (my_bid == 'rock' && computer_bid == 'paper') { notification.innerText = ('You lose') }
+    else if (my_bid == 'rock' && computer_bid == 'scisors') { notification.innerText = 'You win'; score_num += 1; score.innerText = score_num }
+    else if (my_bid == 'scisors' && computer_bid == 'paper') { notification.innerText = ('You win'); score_num += 1; score.innerText = score_num }
+    else if (my_bid == 'scisors' && computer_bid == 'rock') { notification.innerText = 'You lose' }
+
+    console.log(my_bid, computer_bid)
+}
+
+paper.addEventListener('click', function () {
+
+    my_bid = 'paper'
+    me.innerText = 'Your Pick:'
+    comp.innerText = 'Computers Pick:'
+
+    
+    if (my_bid === 'paper'){
+        paper.src = paper_src
+    }
+
+    
+    computer_move = Math.floor(Math.random() * 3)
+    computer_bid = computer_choice_str[computer_move]
+
+    
+    if (computer_bid === 'rock') {
+        scisors.src = rock_src
+    } else if (computer_bid === 'paper') {
+        scisors.src = paper_src
+    } else if (computer_bid === 'scisors') {
+        scisors.src = scisors_src
+    }
+
+    
+    rock.remove()
+    stick_1.remove()
+    stick_2.remove()
+    stick_3.remove()
+
+    
+    game_simulation()
+
+    
+    let play_btn = document.createElement('button')
+    play_btn.id = 'again'
+    play_btn.innerText = 'PLAY AGAIN'
+
+
+    if (notification.innerText === 'You lose') {
+        play_btn.style.color = 'red'
+    } else {
+        play_btn.style.color = 'green'
+    }
+
+    
+    play_btn.addEventListener('click', function() {
+        reset_game_ui()
+    })
+
+    document.body.appendChild(play_btn)
+})
+
+rock.addEventListener('click', function () {
+
+    my_bid = 'rock'
+    me.innerText = 'Your Pick:'
+    comp.innerText = 'Computers Pick:'
+
+    if (my_bid === 'rock'){
+        paper.src = rock_src
+    }
+
+
+    computer_move = Math.floor(Math.random() * 3)
+    computer_bid = computer_choice_str[computer_move]
+
+
+    if (computer_bid === 'rock') {
+        scisors.src = rock_src
+    } else if (computer_bid === 'paper') {
+        scisors.src = paper_src
+    } else if (computer_bid === 'scisors') {
+        scisors.src = scisors_src
+    }
+
+
+
+    rock.remove()
+    stick_1.remove()
+    stick_2.remove()
+    stick_3.remove()
+
+
+    game_simulation()
+
+
+    let play_btn = document.createElement('button')
+    play_btn.id = 'again'
+    play_btn.innerText = 'PLAY AGAIN'
+
+    if (notification.innerText === 'You lose') {
+        play_btn.style.color = 'red'
+    } else {
+        play_btn.style.color = 'green'
+    }
+
+    play_btn.addEventListener('click', function(){
+
+        reset_game_ui()
+
+    })
+
+    document.body.appendChild(play_btn)
+
+})
+
+scisors.addEventListener('click', function () {
+
+    my_bid = 'scisors'
+    me.innerText = 'Your Pick:'
+    comp.innerText = 'Computers Pick:'
+
+
+    if (my_bid === 'scisors'){
+        paper.src = scisors_src
+    }
+
+    computer_move = Math.floor(Math.random() * 3)
+    computer_bid = computer_choice_str[computer_move]
+
+
+    if (computer_bid === 'rock') {
+        scisors.src = rock_src
+    } else if (computer_bid === 'paper') {
+        scisors.src = paper_src
+    } else if (computer_bid === 'scisors') {
+        scisors.src = scisors_src
+    }
+
+
+
+    rock.remove()
+    stick_1.remove()
+    stick_2.remove()
+    stick_3.remove()
+
+
+    game_simulation()
+
+
+    let play_btn = document.createElement('button')
+    play_btn.id = 'again'
+    play_btn.innerText = 'PLAY AGAIN'
+
+    if (notification.innerText === 'You lose') {
+        play_btn.style.color = 'red'
+    } else {
+        play_btn.style.color = 'green'
+    }
+
+    play_btn.addEventListener('click', function(){
+
+        reset_game_ui()
+
+    })
+
+    document.body.appendChild(play_btn)
+
+})
 
