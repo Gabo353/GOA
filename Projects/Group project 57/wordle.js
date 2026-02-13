1a0cd51ab1e49645d57063a07e2b3e1e3aa77526
Gabo353 <dumbadzemambo@gmail.com>
2026-02-13 18:20:41 +0400

    GOA

--- Projects/Group project 57/wordle.js
+++ Projects/Group project 57/wordle.js
@@ -35,15 +35,23 @@ console.log(words[randomizer])
 
 for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', function () {
-        if (words_which_guess[current_row].length < 5) {
-            words_which_guess[current_row] += keys[i].innerHTML.toLowerCase()
+        if (i == 19 || i == 27) { return }
+
+        else {
+
+            if (words_which_guess[current_row].length < 5) {
+                words_which_guess[current_row] += keys[i].innerHTML.toLowerCase()
+            }
+
+            for (let j = 0; j < words_which_guess[current_row].length; j++) {
+                brackets[current_row * 5 + j].innerHTML = words_which_guess[current_row][j]
+                brackets[current_row * 5 + j].style.backgroundColor = ''
+            }
+
         }
 
-        for (let j = 0; j < words_which_guess[current_row].length; j++) {
-            brackets[current_row * 5 + j].innerHTML = words_which_guess[current_row][j]
-            brackets[current_row * 5 + j].style.backgroundColor = ''
-        }
-    });
+    })
+
 }
 
 keys[27].addEventListener('click', function () {
@@ -51,7 +59,7 @@ keys[27].addEventListener('click', function () {
 
     let comp_word = words[randomizer].split('')
 
-   
+
     for (let i = 0; i < 5; i++) {
         if (words_which_guess[current_row][i] === comp_word[i]) {
             brackets[current_row * 5 + i].style.backgroundColor = 'lightgreen'
@@ -59,7 +67,7 @@ keys[27].addEventListener('click', function () {
         }
     }
 
-    
+
     for (let i = 0; i < 5; i++) {
         if (brackets[current_row * 5 + i].style.backgroundColor !== 'lightgreen') {
             let index = comp_word.indexOf(words_which_guess[current_row][i])
@@ -72,5 +80,15 @@ keys[27].addEventListener('click', function () {
         }
     }
 
+    current_row++
+})
+
+keys[19].addEventListener('click', function(){
+
+    for (let i = 0; i < keys.length; i++){
+
+        brackets[current_row].innerHTML = ''
+
+    }
     current_row++
 })
