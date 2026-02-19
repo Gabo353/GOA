779d998d901a130dbb65efff4654b2c2c679ff85
Gabo353 <dumbadzemambo@gmail.com>
2026-02-19 21:19:18 +0400

    GOA

--- Level 140/Home/ecommerce-product-page-main/index.js
+++ Level 140/Home/ecommerce-product-page-main/index.js
@@ -1,22 +1,32 @@
 let cart_btn = document.getElementById('cart')
+let toRemove_Add_Cart = 1
 
 cart_btn.addEventListener('click', function () {
 
-    let div = document.createElement('div')
-    let span = document.createElement('span')
-    let span_noty = document.createElement('span')
+    if (toRemove_Add_Cart % 2 == 0) { return }
 
-    div.id = 'added'
-    span.id = 'cart_top'
-    span.innerHTML = 'Cart'
-    span_noty.id = 'empty'
-    span_noty.innerHTML = 'Your cart is empty.'
+    else {
+        let div = document.createElement('div')
+        let span = document.createElement('span')
+        let span_noty = document.createElement('span')
+
+        div.id = 'added'
+        span.id = 'cart_top'
+        span.innerHTML = 'Cart'
+        span_noty.id = 'empty'
+        span_noty.innerHTML = 'Your cart is empty.'
 
 
-    div.appendChild(span)
-    div.appendChild(span_noty)
-    document.body.appendChild(div)
+        div.appendChild(span)
+        div.appendChild(span_noty)
+        document.body.appendChild(div)
+    }
+
+    toRemove_Add_Cart++
+
 })
+
+//-----------------------------plus_minus_btn-----------------------------------
 
 let plus = document.getElementById('plus')
 let minus = document.getElementById('minus')
@@ -34,7 +44,10 @@ minus.addEventListener('click', function () {
             counter.innerHTML = num += -1
         }
     }
-})  
+})
+
+
+//-----------------------------img_change-----------------------------------
 
 let main = document.getElementById('main_img')
 
@@ -43,15 +56,101 @@ let img_2 = document.getElementById('product_2')
 let img_3 = document.getElementById('product_3')
 let img_4 = document.getElementById('product_4')
 
-img_4.addEventListener('click', function(){
-    main.src = img_4.src
+let images = [img_1, img_2, img_3, img_4]
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function () {
+        main.src = this.src
+        for (let j = 0; j < images.length; j++) {
+            images[j].classList.remove('selected')
+        }
+        this.classList.add('selected')
+
+    })
+}
+
+
+//-----------------------------Scroll_img-----------------------------------
+
+main.addEventListener('click', function () {
+
+    let img_add = document.createElement('img')
+    let div_bg = document.createElement('div')
+    let button_left = document.createElement('button')
+    let button_right = document.createElement('button')
+    let img_right = document.createElement('img')
+    let img_left = document.createElement('img')
+
+    img_right.src = './images/icon-next.svg'
+    img_left.src = './images/icon-next.svg'
+
+    img_add.src = main.src
+    img_add.className = 'new_main'
+    div_bg.classList = 'grey_bg'
+    button_right.classList = 'right_btn'
+    button_left.classList = 'left_btn'
+
+    button_right.appendChild(img_right)
+    button_left.appendChild(img_left)
+
+
+    document.body.appendChild(img_add)
+    document.body.appendChild(div_bg)
+    document.body.appendChild(button_right)
+    document.body.appendChild(button_left)
+
+    let img_2_1 = document.createElement('img')
+    let img_2_2 = document.createElement('img')
+    let img_2_3 = document.createElement('img')
+    let img_2_4 = document.createElement('img')
+    
+    img_2_1.src = img_1.src
+    img_2_2.src = img_2.src
+    img_2_3.src = img_3.src
+    img_2_4.src = img_4.src
+
+    img_2_1.id = 'product_1'
+    img_2_2.id = 'product_2'
+    img_2_3.id = 'product_3'
+    img_2_4.id = 'product_4'
+
+    img_2_1.style.width = '80px'
+    img_2_2.style.width = '80px'
+    img_2_3.style.width = '80px'
+    img_2_4.style.width = '80px'
+
+    img_2_1.style.position = 'absolute'
+    img_2_1.style.left = '37.5%'
+    img_2_1.style.bottom = '2%'
+    img_2_1.style.borderRadius = '8px'
+
+    img_2_2.style.position = 'absolute'
+    img_2_2.style.left = '44.5%'
+    img_2_2.style.bottom = '2%'
+    img_2_2.style.borderRadius = '8px'
+
+    img_2_3.style.position = 'absolute'
+    img_2_3.style.left = '51.5%'
+    img_2_3.style.bottom = '2%'
+    img_2_3.style.borderRadius = '8px'
+
+    img_2_4.style.position = 'absolute'
+    img_2_4.style.left = '58.5%'
+    img_2_4.style.bottom = '2%'
+    img_2_4.style.borderRadius = '8px'
+
+    document.body.appendChild(img_2_1)
+    document.body.appendChild(img_2_2)
+    document.body.appendChild(img_2_3)
+    document.body.appendChild(img_2_4)
+
+    button_left.addEventListener('click', function () {
+
+
+    })
+    button_right.addEventListener('click', function () {
+
+
+    })
 })
-img_3.addEventListener('click', function(){
-    main.src = img_3.src
-})
-img_2.addEventListener('click', function(){
-    main.src = img_2.src
-})
-img_1.addEventListener('click', function(){
-    main.src = img_1.src
-})
+
